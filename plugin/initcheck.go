4af33745111b2ad0a4da8e32d21f6c5382b4731b
Zhe Chen <zhe.chen@epidemicsound.com>
2024-02-17 20:37:00 +0100

    fix lint issue

--- plugin/initcheck.go
+++ plugin/initcheck.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
+// New plugin, follow doc https://golangci-lint.run/contributing/new-linters/#configure-a-plugin
 func New(conf any) ([]*analysis.Analyzer, error) {
 
 	fmt.Printf("My configuration (%[1]T): %#[1]v\n", conf)
